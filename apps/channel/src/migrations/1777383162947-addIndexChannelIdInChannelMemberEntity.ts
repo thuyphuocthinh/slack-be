072a746a461587dd169278705b8be0df72793095
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2026-04-29 07:05:08 +0700

    fix: add migrations in channel

---
+++ apps/channel/src/migrations/1777383162947-addIndexChannelIdInChannelMemberEntity.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddIndexChannelIdInChannelMemberEntity1777383162947 implements MigrationInterface {
+    name = 'AddIndexChannelIdInChannelMemberEntity1777383162947'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE INDEX "IDX_71a10831469775a1effdd85f24" ON "channel_members" ("channel_id") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_71a10831469775a1effdd85f24"`);
+    }
+
+}
---
+++ apps/channel/src/migrations/1777383380139-denormalizeChannelMember.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class DenormalizeChannelMember1777383380139 implements MigrationInterface {
+    name = 'DenormalizeChannelMember1777383380139'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "channel_members" ADD "email" character varying(255)`);
+        await queryRunner.query(`ALTER TABLE "channel_members" ADD "first_name" character varying(100)`);
+        await queryRunner.query(`ALTER TABLE "channel_members" ADD "last_name" character varying(100)`);
+        await queryRunner.query(`ALTER TABLE "channel_members" ADD "avatar_url" text`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "channel_members" DROP COLUMN "avatar_url"`);
+        await queryRunner.query(`ALTER TABLE "channel_members" DROP COLUMN "last_name"`);
+        await queryRunner.query(`ALTER TABLE "channel_members" DROP COLUMN "first_name"`);
+        await queryRunner.query(`ALTER TABLE "channel_members" DROP COLUMN "email"`);
+    }
+
+}
