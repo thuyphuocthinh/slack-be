ee900f798b66295d3b5136cfd5d664ad90a014c8
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2026-05-28 17:04:40 +0700

    fix: reduce rate limit of webhook receiver from 1000 secs to 1sec

--- apps/api-gateway/src/webhook-receiver/webhook-receiver.controller.ts
+++ apps/api-gateway/src/webhook-receiver/webhook-receiver.controller.ts
@@ -8,11 +8,11 @@ import { WebhookReceiverService } from './webhook-receiver.service';
 @ApiTags('Incoming Webhooks')
 @Controller('services/hooks')
 export class WebhookReceiverController {
-  constructor(private readonly webhookReceiverService: WebhookReceiverService) {}
+  constructor(private readonly webhookReceiverService: WebhookReceiverService) { }
 
   @Post(':workspaceId/:channelId/:token')
   @Public()
-  @RateLimit({ limit: 1, window: 1000 }) // 1 request per second
+  @RateLimit({ limit: 1, window: 1 }) // 1 request per second
   @HttpCode(200)
   @ApiOperation({ summary: 'Receive incoming webhook payload from external services' })
   @ApiResponse({ status: 200, description: 'Payload received and queued successfully' })
