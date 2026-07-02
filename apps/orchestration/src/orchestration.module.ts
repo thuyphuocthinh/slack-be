4b355621024e76b64f21fc885ec942b4cef7641c
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2026-07-02 10:12:37 +0700

    fix: fix import config module in orchestration

--- apps/orchestration/src/orchestration.module.ts
+++ apps/orchestration/src/orchestration.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 import { QueueModule, EQueueName } from '@slack/queue';
 import { NAME_SERVICE_TCP, PORT_TCP } from '@slack/constants';
 import { getMicroserviceClientConfig } from '@slack/common';
@@ -12,6 +13,7 @@ import { MessageClientService } from './message-client.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     QueueModule.forRoot(),
     QueueModule.forFeature([EQueueName.AI_ORCHESTRATION_QUEUE, EQueueName.SOCKET_QUEUE]),
     ClientsModule.registerAsync([
